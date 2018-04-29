b9239c76f0d1c9d1ae5b0b8067ac422f76bb2e8b
Karlito <karlo.dimjasevic@gmail.com>
2018-04-29 22:23:09 +0200

    Added commands for 'Next >>>' and 'Close' buttons. Added very complicated but useful box/grid sizer system. Disadvantage: cannot change between two windows after clicking 'Next >>>'.

--- installer/F1 1998 Mod Setup.py
+++ installer/F1 1998 Mod Setup.py
@@ -19,59 +19,85 @@ import wx
 import os
 import time
 
-# CWD = os.path.dirname(os.path.realpath('F1 1998 Mod Setup.py'))
+# CWD = os.path.dirname(os.path.realpath('F1 1998 Mod Setup.py'))  # fix !
 # print(CWD)
 
-CWP = 'E:/Users/Karlo/Race Department/Programs/F1 1998 Mod Installer v1.0'
+CWP = 'E:/Users/Karlo/Race Department/Programs/F1-1998-Season-Mod'  # temp !
 
 
 class GUI(wx.Frame):
     
     def __init__(self, Parent, Title):
-        super(GUI, self).__init__(Parent, title=Title, size=(600, 450))
+        super(GUI, self).__init__(Parent, title=Title, style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER, size=(600, 450))
+        
+        self.Panel = wx.Panel(self)
+        
+        self.MainBox = wx.BoxSizer(wx.VERTICAL)
+        self.MainSizer = wx.GridBagSizer(10, 10)
+        
+        self.ImageBox = wx.BoxSizer(wx.HORIZONTAL)
+        self.ImageSizer = wx.GridBagSizer(0, 0)
+        self.TitleBox = wx.BoxSizer(wx.HORIZONTAL)
+        self.TitleSizer = wx.GridBagSizer(0, 0)
+        self.InterFaceBox = wx.BoxSizer(wx.HORIZONTAL)
+        self.InterFaceSizer = wx.GridBagSizer(0, 0)
+        self.ButtonsBox = wx.BoxSizer(wx.HORIZONTAL)
+        self.ButtonsSizer = wx.GridBagSizer(0, 0)
         
         self.IntroduceWindow()
+        
+        self.SetMinSize((600, 450))
+        self.SetMaxSize((600, 450))
         self.Centre()
-        self.Show()
-        
-        
-    def IntroduceWindow(self):
-        Font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
-        Font.SetPointSize(200)
-        
-        Panel = wx.Panel(self)
-        VerBox = wx.BoxSizer(wx.VERTICAL)
-        
-        Sizer = wx.GridBagSizer(30, 30)
-        
-        ModImage = wx.StaticBitmap(Panel, bitmap=wx.Bitmap(u'{}{}'.format(CWP, '/gui/image.bmp')))
-        Sizer.Add(ModImage, pos=(0, 0), span=(2, 0), flag=wx.EXPAND)
-        
+        self.Show()        
+
+    def IntroduceWindow(self):        
+        ModImage = wx.StaticBitmap(self.Panel, bitmap=wx.Bitmap(u'{}{}'.format(CWP, '/installer/gui/image.bmp')))        
         ModTitleFont = wx.Font(18, wx.DEFAULT, wx.ITALIC, wx.BOLD)
-        ModTitle = wx.StaticText(Panel, label="Welcome to\nF1 1998 Mod Setup")
-        ModTitle.SetFont(ModTitleFont)
-        Sizer.Add(ModTitle, pos=(0, 1), span=(0, 2), flag=wx.LEFT|wx.RIGHT|wx.EXPAND, border=30)
+        ModTitle = wx.StaticText(self.Panel, label="{}\n{}".format("Welcome to", "F1 1998 Mod Setup"))
+        ModTitle.SetFont(ModTitleFont)         
+        IntroText = wx.StaticText(self.Panel, label="Intro Text Here")        
+        Line = wx.StaticLine(self.Panel)
+        NextButton = wx.Button(self.Panel, label="Next >>>", size=(100, 30))
+        CloseButton = wx.Button(self.Panel, label="Close", size=(100, 30))
+        BackButton = wx.Button(self.Panel, label="Back <<<", size=(100, 30))
+
+        self.ImageSizer.Add(ModImage, pos=(0, 0))
+        self.TitleSizer.Add(ModTitle, pos=(0, 0))
+        self.InterFaceSizer.Add(IntroText, pos=(0, 0), flag=wx.ALL, border=20)        
+        self.ButtonsSizer.Add(Line, pos=(0, 0), span=(0, 3), flag=wx.EXPAND)
+        self.ButtonsSizer.Add(BackButton, pos=(1, 0), flag=wx.RIGHT, border=254)
+        self.ButtonsSizer.Add(CloseButton, pos=(1, 2))
+        self.ButtonsSizer.Add(NextButton, pos=(1, 1), flag=wx.RIGHT, border=10)
         
-        IntroTextMessage = """
-        Here, intro text!
-        """
-        IntroText = wx.StaticText(Panel, label=IntroTextMessage)
-        Sizer.Add(IntroText, pos=(1, 1), span=(0, 2), flag=wx.EXPAND)
+        self.ImageBox.Add(self.ImageSizer)
+        self.TitleBox.Add(self.TitleSizer, flag=wx.LEFT|wx.RIGHT, border=45)
+        self.InterFaceBox.Add(self.InterFaceSizer)
+        self.ButtonsBox.Add(self.ButtonsSizer)
         
-        Line = wx.StaticLine(Panel)
-        Sizer.Add(Line, pos=(2, 0), span=(0, 3), flag=wx.EXPAND, border=0)
+        self.MainSizer.Add(self.ImageBox, pos=(0, 0), span=(2, 0))
+        self.MainSizer.Add(self.TitleBox, pos=(0, 1))
+        self.MainSizer.Add(self.InterFaceBox, pos=(1, 1))
+        self.MainSizer.Add(self.ButtonsBox, pos=(2, 0), span=(0, 2))
         
-        NextButton = wx.Button(Panel, label="Next >>>", size=(100, 30))
-        Sizer.Add(NextButton, pos=(3, 1), flag=wx.TOP, border=-13)
+        self.MainBox.Add(self.MainSizer, flag=wx.ALL, border=10)
         
-        CloseButton = wx.Button(Panel, label="Close", size=(100, 30))
-        Sizer.Add(CloseButton, pos=(3, 2), flag=wx.TOP, border=-13)
+        self.Panel.SetSizerAndFit(self.MainBox)
         
-        VerBox.Add(Sizer, flag=wx.ALL, border=15)
-        Panel.SetSizerAndFit(VerBox)
+        self.Bind(wx.EVT_BUTTON, self.OnNextIW, NextButton)
+        self.Bind(wx.EVT_BUTTON, self.OnClose, CloseButton)
+
+    def DirInputWindow(self):
+        pass
+        
+    def OnClose(self, Evt):
+        self.Close()
+    
+    def OnNextIW(self, Evt):
+        self.DirInputWindow()
         
     
 if __name__ == '__main__':
     Application = wx.App()
-    GUI(None, Title='F1 1998 Mod Setup')
+    GUI(None, Title='F1 1998 Wizard')
     Application.MainLoop()
